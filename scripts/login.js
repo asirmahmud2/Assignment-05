6b4eca8ba3e4bf095f4aa11636e79612be4c1d90
Asir Mahmud Ishmam <asirmahmud2@gmail.com>
2026-03-07 13:23:14 +0600

    Log in design and functionality

---
+++ scripts/login.js
@@ -0,0 +1,17 @@
+document.getElementById("login-btn").addEventListener('click',()=>
+{
+    const loginInput=document.getElementById("login-username");
+    const loginPass=document.getElementById("login-password");
+
+    const username= loginInput.value;
+    const password= loginPass.value;
+
+    if(username=="admin" && password=="admin123")
+    {
+        alert("Login Successful");
+        window.location.assign("/home.html");
+    }
+    else{
+        alert("Wrong Username or Password")
+    }
+})
